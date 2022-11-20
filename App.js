d69f2bdf7363bee21f49a687b04ebb30fe42a56f
Tinyefuza <gordon.tinyefuza@gmail.com>
2022-11-20 12:37:40 +0300

    Finish Home Screen

--- App.js
+++ App.js
@@ -2,41 +2,106 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('./assets/images/logo.png')}
-      />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}>Contact Us</Text>
-      </TouchableOpacity>
+    <View>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require('./assets/images/logo.png')}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Contact Us</Text>
+          <Image
+          style={styles.contact}
+          source={require('./assets/images/contact-icon.png')}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.firstSectionText}>
+        <Text style={styles.textOne}>Coming Soon</Text>
+        <Text style={styles.textTwo}>Bringing Muslims Together</Text>
+      </View>
+      <View style={styles.socialImages}>
+        <Image
+          style={styles.social}
+          source={require('./assets/images/google-play.png')}
+        />
+        <Image
+          style={styles.social}
+          source={require('./assets/images/apple-store.png')}
+        />
+      </View>
+      <View style={styles.imageOneContainer}>
+        <Image
+            style={styles.landingImageOne}
+            source={require('./assets/images/landing-image-2.png')}
+        />
+      </View>
+      <View style={styles.firstSectionText}>
+        <Text style={styles.textOne}>Coming Soon</Text>
+        <Text style={styles.textTwo}>Get Notified When we Launch</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     flexDirection: 'row',
-    // alignItems: 'center',
     justifyContent: 'space-between',
     margin: 20
   },
   logo: {
-    width: 74, 
+    width: 49, 
     color: 'blue', 
-    height: 15,
-    marginTop: 18
+    height: 13,
+    marginTop: 10
   },
   button: {
     backgroundColor: 'black',
     borderRadius: 6,
-    width: 90,
-    height: 50,
+    width: 97,
+    height: 32,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    padding: 4
   },
   text: {
     color: 'white',
+  },
+  contact: {
+    color: 'white',
+    width: 10,
+    height: 10,
+  },
+  firstSectionText: {
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 30
+  },
+  textOne: {
+    fontSize: 12,
+    fontWeight: 500
+  },
+  textTwo: {
+    fontSize: 36,
+    fontWeight: 800
+  },
+  socialImages: {
+    marginTop: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  social: {
+    width: 134,
+    height: 38
+  },
+  imageOneContainer: {
+    alignItems: 'center'
+  },
+  landingImageOne: {
+    width: 350,
+    height: 396
   }
 });
